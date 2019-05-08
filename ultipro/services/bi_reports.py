e80b99d9961cbb8e71ff70053e4ffccced2b60ca
Jonathan Revah <jonathan.revah@equinox.com>
2019-05-08 13:13:33 -0400

    converted try/except to @backoff decorator.

--- ultipro/services/bi_reports.py
+++ ultipro/services/bi_reports.py
@@ -1,19 +1,12 @@
-import time
+import backoff
 
-from zeep import Client as ZeepClient
+from ultipro.helpers import backoff_hdlr
 from ultipro.services import bi_data, bi_stream
 
+@backoff.on_exception(backoff.expo, AttributeError, max_tries=10, on_backoff=backoff_hdlr)
 def execute_and_fetch(client, report_path, delimiter=','):
+    print("execute and fetch")
     context = bi_data.log_on_with_token(client)
     k = bi_data.execute_report(client, context, report_path, delimiter=delimiter)
-    n = 1
-    while n <= 10:
-        try:
-            r = bi_stream.retrieve_report(client, k)
-            resp = r['body']['ReportStream'].decode('unicode-escape')
-        except AttributeError:
-            print('tries: {}'.format(n))
-            time.sleep(5)
-            n += 1
-        else:
-            return resp
+    r = bi_stream.retrieve_report(client, k)
+    return r['body']['ReportStream'].decode('unicode-escape')
