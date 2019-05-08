2a28f7e11383fed007f53f79eed515ee5d1d9cfc
Jonathan Revah <jonathan.revah@equinox.com>
2019-05-08 12:43:48 -0400

    added a try/except in case the report hasn't finished running. TODO: implement the same thing using backoff for consistency

--- ultipro/services/bi_reports.py
+++ ultipro/services/bi_reports.py
@@ -1,9 +1,19 @@
+import time
+
 from zeep import Client as ZeepClient
 from ultipro.services import bi_data, bi_stream
 
 def execute_and_fetch(client, report_path, delimiter=','):
     context = bi_data.log_on_with_token(client)
     k = bi_data.execute_report(client, context, report_path, delimiter=delimiter)
-    r = bi_stream.retrieve_report(client, k)
-    return r['body']['ReportStream'].decode('unicode-escape')
-
+    n = 1
+    while n <= 10:
+        try:
+            r = bi_stream.retrieve_report(client, k)
+            resp = r['body']['ReportStream'].decode('unicode-escape')
+        except AttributeError:
+            print('tries: {}'.format(n))
+            time.sleep(5)
+            n += 1
+        else:
+            return resp
