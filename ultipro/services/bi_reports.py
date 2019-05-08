7cf3b54a069c65fcc47c864b3eb414a26132d726
Jonathan Revah <jonathan.revah@equinox.com>
2019-05-08 13:57:14 -0400

    moved the backoff decorator to check if the report's status is 'WORKING' and keep trying indefinitely

--- ultipro/services/bi_reports.py
+++ ultipro/services/bi_reports.py
@@ -1,11 +1,7 @@
-import backoff
-
-from ultipro.helpers import backoff_hdlr
 from ultipro.services import bi_data, bi_stream
 
-@backoff.on_exception(backoff.expo, AttributeError, max_tries=10, on_backoff=backoff_hdlr)
+
 def execute_and_fetch(client, report_path, delimiter=','):
-    print("execute and fetch")
     context = bi_data.log_on_with_token(client)
     k = bi_data.execute_report(client, context, report_path, delimiter=delimiter)
     r = bi_stream.retrieve_report(client, k)
